8cd5befb6117fddbe99205e2843173d845370470
willrkn <willrkn@users.noreply.github.com>
2020-01-10 18:35:31 +0000

    Added server side image uploading
    TODO: Login Page

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+ini_set( "display_errors", true );
+date_default_timezone_set( "Europe/London" );  // http://www.php.net/manual/en/timezones.php
+define( "DB_DSN", "mysql:host=localhost;dbname=cms" );
+define( "DB_USERNAME", "root" );
+define( "DB_PASSWORD", "" );
+define( "CLASS_PATH", "classes" );
+define( "TEMPLATE_PATH", "templates" );
+define( "HOMEPAGE_NUM_ARTICLES", 3 );
+define( "ADMIN_USERNAME", "jess" );
+define( "ADMIN_PASSWORD", "jess" );
+require( CLASS_PATH . "/Article.php" );
+
+function handleException( $exception ) {
+  echo "Sorry, a problem occurred. Please try later.";
+  error_log( $exception->getMessage() );
+}
+
+set_exception_handler( 'handleException' );
+?>
---
+++ includes/dbh.php
@@ -0,0 +1,9 @@
+<?php
+
+// change when on online server
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gallery";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
